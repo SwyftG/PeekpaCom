e26de44af0539147bcdcde20428b888fa60a596a
lg1889 <lg1889@nyu.edu>
2020-07-10 19:47:32 +0900

    Advanced_02

--- apps/datacenter/jpearth_view.py
+++ apps/datacenter/jpearth_view.py
@@ -11,15 +11,18 @@ from django.core.paginator import Paginator
 @method_decorator(peekpa_code_required, name='get')
 @method_decorator(peekpa_tracking, name='get')
 class JpEarthView(View):
-
-    client = pymongo.MongoClient("mongodb+srv://peekpa-user:peekpa2020@peekpa.ofyco.mongodb.net/Peekpa?retryWrites=true&w=majority",
+    TYPE_ALL = 1
+    TYPE_SEARCH = 2
+    client = pymongo.MongoClient(
+        "mongodb+srv://peekpa-user:peekpa2020@peekpa.ofyco.mongodb.net/Peekpa?retryWrites=true&w=majority",
         ssl_cert_reqs=ssl.CERT_NONE)
     db = client['PeekpaMongoData']
     collection = db['JpEarth']
 
     def get(self, request):
         page = int(request.GET.get('p', 1))
-        list_data = self.collection.find()
+        handle_type, search_key = self.process_paramter(request)
+        list_data = self.get_data_from_db(handle_type, search_key)
         paginator = Paginator(list(list_data), 15)
         page_obj = paginator.page(page)
         context = {
@@ -27,6 +30,32 @@ class JpEarthView(View):
         }
         context.update(self.get_pagination_data(paginator, page_obj))
         return render(request, 'datacenter/jpearth/manage.html', context=context);
+
+    def get_data_from_db(self, handle_type, search_key):
+        if handle_type == self.TYPE_ALL:
+            # 搜索全部结果
+            result = self.collection.find()
+
+            # 降序排列
+            #result = self.collection.find().sort('jp_time_num', pymongo.DESCENDING)
+
+            # 升序排列
+            # result = self.collection.find().sort('jp_time_num', pymongo.ASCENDING)
+
+            # 只 返回 地震地点 还有 地震时间
+            # result = self.collection.find({},{'jp_title', 'jp_location'})
+
+            # 不 返回 地震地点 还有 地震时间
+            # result = self.collection.find({},{'jp_title':False, 'jp_location':False})
+
+            # find_one()单个数据搜索
+            #result = self.collection.find_one({"jp_location":"福島県沖"})
+        else:
+            # 通过地址(jp_location) 精确查找
+            #result = self.collection.find({"jp_location": search_key})
+            # 通过地址(jp_location) 模糊查找
+            result = self.collection.find({"jp_location": {'$regex': ".*" + search_key + ".*"}})
+        return list(result)
 
     def get_pagination_data(self, paginator, page_obj, around_count=2):
         current_page = page_obj.number
@@ -58,3 +87,7 @@ class JpEarthView(View):
             'num_pages': num_pages
         }
 
+    def process_paramter(self, request):
+        search_key = request.GET.get('search')
+        handle_type = self.TYPE_ALL if search_key is None else self.TYPE_SEARCH
+        return handle_type, search_key
